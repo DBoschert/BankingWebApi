403e543102ed1e2f73c4311587a84d9ad7b81584
DrewBoschert <drew.r.boschert@gmail.com>
2023-09-12 10:09:36 -0400

    Added Customers again

---
+++ BankingWebApi/Data/BankingWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BankingWebApi.Models;
+
+namespace BankingWebApi.Data
+{
+    public class BankingWebApiContext : DbContext
+    {
+        public BankingWebApiContext (DbContextOptions<BankingWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BankingWebApi.Models.Customer> Customer { get; set; } = default!;
+    }
+}
